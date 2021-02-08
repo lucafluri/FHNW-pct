3beddda347c74e70b0a0e4c1ed5c8b88bac20f2a
Luca Fluri <luca.fluri4@gmail.com>
2021-02-08 21:00:47 +0100

    accepted lcd display

--- src/pct_course/chapter_1/p706_LCD_Display.cpp
+++ src/pct_course/chapter_1/p706_LCD_Display.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 #include <unistd.h>
-#include <vector>
-#include <string>
-
 
 #include "../../my_libs/local_testing/string_in_out_testing.h" //IDE ONLY
 
@@ -33,27 +30,30 @@ bool segments[][7] = {
     {1, 1, 1, 1, 0, 1, 1}, //9
 };
 
-void drawNumbers(vector<int> &digits, int s){
+void drawNumbers(int (&digits)[8], int s, int max){
     int it[] = {0, 5, 1, 6, 4, 2, 3}; //Iteration direction Top-Bottom
     int len = *(&it + 1) - it;
     for(int j = 0; j < len; j++){ //Iterate over all 5 segment depths
         int segment = it[j];
          //Iterate over numbers
         if((segment == 0 || segment == 3 || segment== 6)){ //horizontal segments
-            for(int i = 0; i<digits.size(); i++){
+            for(int i = 0; i<=max; i++){
+                if(digits[i] == -1) continue;
                 cout << " ";
                 if(segments[digits[i]][segment]){
                     for(int k = 0; k<s; k++) cout << "-";
                 }else{
                     for(int k = 0; k<s; k++) cout << " ";
                 }
-                if(i != digits.size()-1) cout << "  ";
+                if(i != max) cout << "  ";
                 else cout << " ";
             }
             cout << endl;
         }else if(segment == 5 || segment == 4){ // vertical segments
             for(int k = 0; k<s; k++){
-                for(int i = 0; i<digits.size(); i++){
+                for(int i = 0; i<=max; i++){
+                    if(digits[i] == -1) continue;
+
                     // Segment Left
                     if(segments[digits[i]][segment]){
                         cout << "|";
@@ -63,10 +63,10 @@ void drawNumbers(vector<int> &digits, int s){
                     for(int k = 0; k<s; k++) cout << " ";
                     // Segment Right
                     if(segments[digits[i]][it[j+1]]){
-                        if(i != digits.size()-1) cout << "| ";
+                        if(i != max) cout << "| ";
                         else cout << "|";
                     }else{
-                        if(i != digits.size()-1) cout << "  ";
+                        if(i != max) cout << "  ";
                         else cout << " ";
                     }
                 }
@@ -80,22 +80,23 @@ int main() {
     prepare_ide("p706");     // For IDE only
 
     int s;
-    char n[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    vector<int> digits;
+    char n[8];
+    int digits[8] = {-1, -1, -1, -1, -1, -1, -1, -1};
 
-    while(cin >> s >> n){
-        if(s==0 && n==0) break;
+    int maxIdx = 7;
+
+    while(cin >> s >> n && (s!=0 && n!=0)){
         // Get seperate digits, from back to front
-        for(int i = 0; i < 8; i++) {
-            if(n[i] != 0)
-            digits.push_back(n[i] - '0');
+        for(int i = 0; i < 8 && n[i] != 0; i++) {
+            digits[i] = n[i] - '0';
+            maxIdx = i;
         }
 
-        drawNumbers(digits, s);
+        drawNumbers(digits, s, maxIdx);
         cout << endl;
         // Reset char array
         fill(n, n + 8, 0);
-        digits.clear();
+        fill(digits, digits + 8, -1);
     }
 
     execute_tests();     // For IDE only
