5643a1c25c588be5ea7894593440835e61f8cc97
Luca Fluri <luca.fluri4@gmail.com>
2021-02-08 14:02:31 +0100

    working trip and lcd prep

---
+++ src/pct_course/chapter_1/p706_LCD_Display.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unistd.h>
+
+
+#include "../../my_libs/local_testing/string_in_out_testing.h" //IDE ONLY
+
+
+using namespace std;
+
+int main() {
+    prepare_ide("p706");     // For IDE only
+
+
+    
+    
+
+
+    execute_tests();     // For IDE only
+}
+
