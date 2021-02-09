b8a6f30e6e13f1c5d62450a85f1ea98fb8b14f9e
Luca Fluri <luca.fluri4@gmail.com>
2021-02-09 17:07:01 +0100

    prep for check the check

---
+++ src/pct_course/chapter_1/p10196_checkcheck.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unistd.h>
+
+
+#include "../../my_libs/local_testing/string_in_out_testing.h" //IDE ONLY
+
+
+using namespace std;
+
+int main() {
+    prepare_ide("p10196");     // For IDE only
+
+
+
+    execute_tests();     // For IDE only
+}
+
