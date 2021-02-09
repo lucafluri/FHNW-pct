69be88d488d4cc0f5c95a74696e4d35e53662512
Luca Fluri <luca.fluri4@gmail.com>
2021-02-09 03:24:36 +0100

    prep for interpreter

---
+++ src/pct_course/chapter_1/p10033_interpreter.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unistd.h>
+
+
+#include "../../my_libs/local_testing/string_in_out_testing.h" //IDE ONLY
+
+
+using namespace std;
+
+int main() {
+    prepare_ide("p10033");     // For IDE only
+
+
+    
+
+    execute_tests();     // For IDE only
+}
+
