fcea1a5e9cc7a5fe891329d9a0b8895afb14e56d
lucafluri <me@lucafluri.ch>
2021-06-13 18:12:32 +0200

    prep p705

---
+++ src/pct_course/chapter_9/p705_slashMaze.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <unistd.h>
+#include "../../my_libs/local_testing/string_in_out_testing.h"
+
+using namespace std;
+
+
+int main() {
+    prepare_ide("p705");     // For IDE only
+
+    execute_tests();     // For IDE only
+}
+
