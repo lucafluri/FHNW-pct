5544ba4bccca51e8a8fcc0207bd83ad6afc336c6
lucafluri <me@lucafluri.ch>
2021-06-18 01:22:59 +0200

    prep p10029

---
+++ src/pct_course/chapter_9/p10029_editStepLadder.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstring>
+#include <string>
+
+#include "../../my_libs/local_testing/string_in_out_testing.h"
+
+using namespace std;
+
+
+int main() {
+    prepare_ide("p10029");     // For IDE only
+    // Edit step: add, delete, change
+
+    // Build map with
+
+    execute_tests();     // For IDE only
+}   
