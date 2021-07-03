fb779679eda7122a43ac0079c611781dbcea70a9
lucafluri <me@lucafluri.ch>
2021-07-03 02:11:23 +0200

    prep hanoitowers

---
+++ src/pct_course/chapter_9/p10276_hanoiTower.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <unistd.h>
+
+#include "../../my_libs/local_testing/string_in_out_testing.h"
+
+using namespace std;
+
+int main() {
+    prepare_ide("p10276");     // For IDE only
+    
+
+
+    execute_tests();     // For IDE only
+}   
